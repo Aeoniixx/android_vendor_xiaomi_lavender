e3b2e02e6d8d88f51b085c93d9057d8d7ffcf93a
sabarop <sabar.pakpahan@gmail.com>
2025-03-23 23:09:35 +0700

    lavender: Commonize libmmcamera_dbg

--- lavender-vendor.mk
+++ lavender-vendor.mk
@@ -857,7 +857,6 @@ PRODUCT_PACKAGES += \
     libmmcamera_arcsoftbokeh_lib \
     libmmcamera_atmel_at24c32e_eeprom \
     libmmcamera_bokeh \
-    libmmcamera_dbg \
     libmmcamera_depth_map \
     libmmcamera_dummyalgo \
     libmmcamera_eebinparse \
